f60e4e722505f7e09a2c48a6297ae49cc2a0e8a8
jahid1604091 <jh409780@gmail.com>
2023-08-14 12:36:46 +0600

    proxy url removed and set in apiSlice

--- src/slices/apiSlice.js
+++ src/slices/apiSlice.js
@@ -1,30 +1,45 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-const baseQuery = fetchBaseQuery({ baseUrl: '' });
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+const baseQuery = fetchBaseQuery({
+  baseUrl: "http://localhost:5000",
+  headers: {
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Credentials":true,
+  },
+  credentials: "include",
+  SameSite:"None",
+  Secure:true
+});
 
 export const apiSlice = createApi({
-    reducerPath: 'api',
-    baseQuery,
-    tagTypes: ['User', 'Doctors'],
-    endpoints: (builder) => ({})
+  reducerPath: "api",
+  baseQuery,
+  tagTypes: ["User", "Doctors"],
+  endpoints: (builder) => ({}),
 });
 
 export const countryApiSlice = createApi({
-    reducerPath: 'api2',
-    baseQuery: fetchBaseQuery({
-        baseUrl: 'https://parseapi.back4app.com/classes/BD_BD?count=1',
-        credentials: 'include',
-        prepareHeaders: headers => {
-            headers.set('X-Parse-Application-Id', 'YzDylnYKFUzln2bDr3GNHuQjnD2oXIDTKfO52vSw')
-            headers.set('X-Parse-REST-API-Key', 'LDMYcw2DF06WKpo5gC4Tkqyzmb83tiGDSheCHzw4')
-            return headers;
-        }
+  reducerPath: "api2",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://parseapi.back4app.com/classes/BD_BD?count=1",
+    credentials: "include",
+    prepareHeaders: (headers) => {
+      headers.set(
+        "X-Parse-Application-Id",
+        "YzDylnYKFUzln2bDr3GNHuQjnD2oXIDTKfO52vSw"
+      );
+      headers.set(
+        "X-Parse-REST-API-Key",
+        "LDMYcw2DF06WKpo5gC4Tkqyzmb83tiGDSheCHzw4"
+      );
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getCountryStateCity: builder.query({
+      query: () => ``,
+      providesTags: ["User"],
     }),
-    endpoints: (builder) => ({
-        getCountryStateCity: builder.query({
-            query: () => ``,
-            providesTags: ['User']
-        }),
-    }),
-})
+  }),
+});
 
 export const { useGetCountryStateCityQuery } = countryApiSlice;
--- vite.config.js
+++ vite.config.js
@@ -6,13 +6,13 @@ export default defineConfig({
   plugins: [react()],
   server:{
     port:3000,
-    proxy:{
-      '/api':{
-        target:'https://fair-blue-bat-sock.cyclic.app',
-        // target:'http://localhost:5000',
-        changeOrigin:true,
-        secure:false
-      }
-    }
+    // proxy:{
+    //   '/api':{
+    //     target:'https://fair-blue-bat-sock.cyclic.app',
+    //     // target:'http://localhost:5000',
+    //     changeOrigin:true,
+    //     secure:false
+    //   }
+    // }
   }
 })
